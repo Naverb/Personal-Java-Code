2a49ab8f3cd45f2680bcc196941b5a9c4f2bd03d
Naverb <brevanellefsen@gmail.com>
2014-10-13 19:57:16 -0500

    First Java Files
    Initial Upload of Files

---
+++ Prime Numbers/src/prime/numbers/PrimeNumbers.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prime.numbers;
+import java.util.Arrays;
+/**
+ *
+ * @author Ellefsen
+ */
+public class PrimeNumbers {
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        int[] intArray = new int[] {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(intArray));
+    }
+    
+}
